5fc47ac48205354a694956de1883b793b0ddf90c
Lukas Bai <bailu@bailu.ch>
2025-03-19 06:18:31 +0100

    Fix init script and default config

--- init.sh
+++ init.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-otel_ver="v0.121.0"
-otel_release="https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/${otel_version}"
+otel_version="0.121.0"
+otel_release="https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v${otel_version}"
 
 test -f debian1/otelcol-contrib.deb || wget ${otel_release}/otelcol-contrib_${otel_version}_linux_amd64.deb -O debian1/otelcol-contrib.deb
-cp debian1/otelcol-contrib.deb debian2/otelcol-debian.deb
+cp debian1/otelcol-contrib.deb debian2/otelcol-contrib.deb
 
 touch debian2/out.json
 touch logstash/out.json
@@ -14,7 +14,6 @@ chmod a+w loki/data
 
 mkdir -p grafana/data
 chmod a+w grafana/data
-
 
 openssl genrsa -out ca.key 4096
 openssl req -x509 -new -nodes -key ca.key -sha256 -days 365 -out ca.crt -subj "/CN=OTel CA"
